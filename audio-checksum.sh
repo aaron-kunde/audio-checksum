ad120a928f31b09bce0e5d5f6d5934d4a0d5172f
Aaron Kunde <aaron.kunde@web.de>
2016-02-07 14:26:19 +0100

    Added dummy version of calculating MD5 checksums

--- audio-checksum.sh
+++ audio-checksum.sh
@@ -4,13 +4,22 @@ print_usage() {
     echo "Usage: $0 FILE"
 }
 
-main() {
+process_args() {
     if [ $# -ne 1 ]
     then
 	echo "ERROR"
 	print_usage
 	exit 1
     fi
+}
+
+calc_checksum() {
+    md5sum "$1"
+}
+
+main() {
+    process_args "$@"
+    calc_checksum "$1"
 }
 
 main "$@"
